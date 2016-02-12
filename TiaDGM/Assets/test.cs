e8d2e8512bbda825820390c23f5ea62ee0cb6a74
Tia <10705836@CS518-13.ad.uvu.edu>
2016-02-11 18:35:58 -0700

    Functions and Variables

--- TiaDGM/Assets/test.cs
+++ TiaDGM/Assets/test.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
-
+public class test : MonoBehaviour 
+{
+	int tiaInt = 33;
 	// Use this for initialization
-	void Start () {
-	
+	void Start(){
+		tiaInt = timesthree (tiaInt);
+		Debug.Log (tiaInt);
 	}
-	
+	int timesthree (int testInt){
+		int three = testInt * 3;
+
+		return three;
+	}
+    }
 	// Update is called once per frame
-	void Update () {
 	
-	}
-}
+
